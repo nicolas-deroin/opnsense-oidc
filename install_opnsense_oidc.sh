66f22cb66a978ca33bcdeaf6f0c0509e70ed6abd
Nicolas Deroin <nicolas.deroin.01@gmail.com>
2026-03-10 14:14:23 -0400

    Update install script with logging

--- install_opnsense_oidc.sh
+++ install_opnsense_oidc.sh
@@ -7,11 +7,16 @@ package_name=os-oidc
 src_path=$home_path/$project_name
 tgt_path=$plugins_path/$project_name
 
-
+echo "Removing existing target path $tgt_path..."
 rm -rf $tgt_path
+echo "Done; uninstalling currently installed package $package_name..."
 yes | pkg remove $package_name
+echo "Done; copying source from '$src_path' to '$tgt_path'..."
 cp -R $src_path $tgt_path
+echo "Done; switching to $tgt_path..."
 cd $tgt_path
+echo "Done; making package..."
 make package
+echo "Done; installing package $tgt_path/work/pkg/*.pkg..."
 pkg add $tgt_path/work/pkg/*.pkg
 exit 0
